00a950969b2b3cb8fb76cbe54c3a73af49b4c317
Erick Rodriguez <erickrod54@gmail.com>
2021-11-28 08:01:49 -0500

    clearing the test routing app

--- routing-app/src/index.js
+++ routing-app/src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <BrowserRouter>
     <React.StrictMode>
+      <div>
+        <h1>React Router testing features</h1>
+      </div>
       <App />
     </React.StrictMode>
   </BrowserRouter>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
